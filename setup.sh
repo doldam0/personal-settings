b2c64a69aeafedca57cf26060a56b306bb00dc1e
doldam0 <jinustar19@gmail.com>
2022-12-13 16:21:44 +0900

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#/bin/zsh
+
+# Install Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install brogrammer theme for iterm
+mkdir -p ~/.iterm && curl https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Brogrammer.itermcolors > ~/.iterm/Brogrammer.itermcolors
+
+# Set vimrc
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+cat <<EOF >> ~/.vimrc
+set hlsearch
+set nu
+set autoindent
+set scrolloff=2
+set wildmode=longest,list
+set ts=4
+set sts=4
+set sw=1
+set autowrite
+set autoread
+set cindent
+set bs=eol,start,indent
+set history=256
+set laststatus=2
+set paste
+set shiftwidth=4
+set showmatch
+set smartcase
+set smarttab
+set smartindent
+set softtabstop=4
+set tabstop=4
+set ruler
+set incsearch
+EOF
+
+# Install brogrammer theme for vim
+mkdir -p ~/.vim/colors
+curl https://raw.githubusercontent.com/marciomazza/vim-brogrammer-theme/master/colors/brogrammer.vim > ~/.vim/colors/brogrammer.vim
+
